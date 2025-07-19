be1caccea677c284f527d77da6e2970708eed87d
sunthesuraj <surajjaud1999@gmail.com>
2025-07-20 00:32:33 +0530

    🌐 Connected frontend to Render backend

--- Frontend/src/common/SummaryApi.js
+++ Frontend/src/common/SummaryApi.js
@@ -1,4 +1,5 @@
-export const baseURL = "http://localhost:8080";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://your-backend-name.onrender.com";
 
 const SummaryApi = {
     register : {
